61ee83c48f1f03dd60e9e9c524dee981a409395e
Febi Dian Saputra <febidiancc321@gmail.com>
2024-06-14 10:24:39 +0700

    membuat fitur auth dengan email dan password atau dengan google atau dengan facebook

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Laravel\Socialite\Facades\Socialite;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Facades\JWTFactory;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        try {
+            $id = $this->idrandom();
+            User::create([
+                'name' => $request->name,
+                'notes_user_id' => $id,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+            ]);
+
+            return response()->json([
+                'message' => 'Successfully created an account.',
+                'status' => 'success'
+            ], Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Failed to create an account.',
+                'status' => 'failed',
+                'th' => $th,
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function login(LoginRequest $request)
+    {
+        try {
+            $credentials = $request->only('email', 'password');
+            $user = User::where('email', '=', $credentials['email'])->firstOrFail();
+
+            if ($user->password === null) {
+                return response()->json([
+                    'errors' => [
+                        'email' => "It appears the account uses a different login method.",
+                    ],
+                    'status' => 'failed'
+                ], Response::HTTP_UNAUTHORIZED);
+            } else if (!$token = auth()->attempt($credentials)) {
+                return response()->json([
+                    'status' => 'failed',
+                    'errors' => [
+                        'email' => ['Email or password is incorrect, please try again.'],
+                    ],
+                ], Response::HTTP_UNAUTHORIZED);
+            }
+
+            $minutes = auth()->factory()->getTTL() * 60;
+            $customClaims = ['sub' => $user->id, 'role' => $user->role];
+            $payload = JWTFactory::customClaims($customClaims)->make();
+            $token = JWTAuth::encode($payload);
+            $cookie = cookie('jwt_token', $token, $minutes, null, null, true, true);
+
+            return response()->json([
+                'message' => 'You have successfully logged in.',
+                'user' => new UserResource($user),
+                'status' => 'success',
+            ], Response::HTTP_OK)->withCookie($cookie);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'errors' => [
+                    'email' => "Email or password is incorrect, please try again.",
+                ],
+                'status' => 'failed'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+    }
+
+    public function redirectToGoogle()
+    {
+        return response()->json([
+            'url' => Socialite::driver('google')->stateless()
+                ->redirect()
+                ->getTargetUrl()
+        ]);
+    }
+
+    public function handleGoogleCallback()
+    {
+        $socialiteUser = Socialite::driver('google')->stateless()->user();
+
+        try {
+            $id = $this->idrandom();
+            $user = User::firstOrCreate(
+                ['google_id' => $socialiteUser->id],
+                [
+                    'name' => $socialiteUser->name,
+                    'notes_user_id' => $id,
+                    'email' => $socialiteUser->email,
+                    'email_verified_at' => now(),
+                ],
+            );
+
+            $minutes = auth()->factory()->getTTL() * 60;
+            $customClaims = ['sub' => $user->id, 'role' => $user->role];
+            $payload = JWTFactory::customClaims($customClaims)->make();
+            $token = JWTAuth::encode($payload);
+            $cookie = cookie('jwt_token', $token, $minutes, null, null, true, true);
+
+            return response()->json([
+                'message' => 'You have successfully logged in.',
+                'user' => new UserResource($user),
+                'status' => 'success',
+            ], Response::HTTP_OK)->withCookie($cookie);
+        } catch (QueryException $th) {
+            return response()->json([
+                'status' => "failed",
+                'th' => $th
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function redirectToFacebook()
+    {
+        return response()->json([
+            'url' => Socialite::driver('facebook')->stateless()
+                ->redirect()
+                ->getTargetUrl()
+        ]);
+    }
+
+    public function handleFacebookCallback()
+    {
+        $socialiteUser = Socialite::driver('facebook')->stateless()->user();
+
+        try {
+            $id = $this->idrandom();
+            $user = User::firstOrCreate(
+                ['facebook_id' => $socialiteUser->id],
+                [
+                    'name' => $socialiteUser->name,
+                    'email' => $socialiteUser->email,
+                    'email_verified_at' => now(),
+                    'notes_user_id' => $this->idrandom(),
+                ],
+            );
+
+            if ($user->status === 0) {
+                return response()->json([
+                    'message' => 'Akun Anda dinonaktifkan.',
+                    'status' => 'failed',
+                ], Response::HTTP_UNAUTHORIZED);
+            } else {
+                $minutes = auth()->factory()->getTTL() * 60;
+                $customClaims = ['sub' => $user->id, 'role' => $user->role];
+                $payload = JWTFactory::customClaims($customClaims)->make();
+                $token = JWTAuth::encode($payload);
+                $cookie = cookie('jwt_token', $token, $minutes, null, null, true, true);
+
+                return response()->json([
+                    'message' => 'Anda Berhasil Login.',
+                    'user' => new UserResource($user),
+                    'status' => 'success',
+                ], Response::HTTP_OK)->withCookie($cookie);
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => "failed",
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function idrandom()
+    {
+        $tanggal = now()->format('dmY');
+        $jam = now()->format('H');
+
+        $randomAngka = mt_rand(1, 99999);
+        $customId = $tanggal . $jam . $randomAngka;
+
+        return $customId;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->prefix('auth')->group(function () {
+    Route::post('register', 'register')->middleware('throttle:6,60')->name('auth.register');
+    Route::post('login', 'login')->middleware('throttle:10,30')->name('auth.login');
+    Route::get('login/google', 'redirectToGoogle')->name('auth.redirectToGoogle');
+    Route::get('login/google/callback', 'handleGoogleCallback')->name('auth.handleGoogleCallback');
+    Route::get('login/facebook', 'redirectToFacebook')->name('auth.redirectToFacebook');
+    Route::get('login/facebook/callback', 'handleFacebookCallback')->name('auth.handleFacebookCallback');
 });
